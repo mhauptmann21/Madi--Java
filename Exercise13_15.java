2fbfdf43757c120bf3bdfc5343a5425a9943ab15
mhauptmann21 <mhauptmann@ivytech.edu>
2025-04-01 10:34:16 -0400

    Created these files

---
+++ Exercise13_15.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Exercise13_15 {
+  public static void main(String[] args) {
+    // Prompt the user to enter two Rational numbers
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter rational r1 with numerator and denominator seperated by a space: ");
+    String n1 = input.next();
+    String d1 = input.next();
+
+    System.out.print("Enter rational r2 with numerator and denominator seperated by a space: ");
+    String n2 = input.next();
+    String d2 = input.next();
+
+    Rational r1 = new Rational(
+      new BigInteger(n1), new BigInteger(d1));
+    Rational r2 = new Rational(
+      new BigInteger(n2), new BigInteger(d2));
+
+    // Display results
+    System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
+    System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
+    System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
+    System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
+    System.out.println(r2 + " is " + r2.doubleValue());
+  }
+}
